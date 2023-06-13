18d8522261b61b6177ec4d9fa483458a4d89746e
NiklasHenningsen <82464581+NiklasHenningsen@users.noreply.github.com>
2023-06-13 16:56:53 +0200

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,53 +1,62 @@
+import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
-from email.utils import COMMASPACE, formatdate
+from email.utils import COMMASPACE
 from email import encoders
-import os
-import time
 from picamera import PiCamera
+from time import sleep
 
 camera = PiCamera()
 
-def send_mail(send_from, send_to, subject, message, files=[],
-              server="mail.gmx.net", port=587, username='', password='',
-              use_tls=True):
+def send_email(image_path):
+    sender = 'niklashenningsen@gmx.net'
+    password = input('Enter your email password: Ralf!2004.')
+    recipient = 'madita.heinisch@gmx.de'
+    subject = 'Raspberry Pi Image'
+    body = 'Image captured by Raspberry Pi'
+    server = 'mail.gmx.net'
+    port = 587
+
     msg = MIMEMultipart()
-    msg['From'] = send_from
-    msg['To'] = COMMASPACE.join(send_to)
-    msg['Date'] = formatdate(localtime=True)
+    msg['From'] = sender
+    msg['To'] = recipient
     msg['Subject'] = subject
 
-    msg.attach(MIMEText(message))
+    msg.attach(MIMEText(body))
 
-    for path in files:
-        part = MIMEBase('application', "octet-stream")
-        with open(path, 'rb') as file:
-            part.set_payload(file.read())
+    with open(image_path, 'rb') as f:
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(f.read())
         encoders.encode_base64(part)
-        part.add_header('Content-Disposition',
-                        'attachment; filename="{}"'.format(os.path.basename(path)))
+        part.add_header('Content-Disposition', 'attachment', filename=os.path.basename(image_path))
         msg.attach(part)
 
     smtp = smtplib.SMTP(server, port)
-    if use_tls:
-        smtp.starttls()
-    smtp.login(username, password)
-    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.starttls()
+    smtp.login(sender, password)
+    smtp.sendmail(sender, recipient, msg.as_string())
     smtp.quit()
 
-# E-Mail-Konfiguration
-email_username = "niklashenningsen@gmx.net"
-email_password = "Ralf!2004."
-email_sender = email_username
-email_recipient = ["niklas_henningsen@icloud.com"]  # Empfänger-E-Mail-Adresse
+def capture_image():
+    image_folder = os.path.join(os.path.expanduser('~'), 'Desktop', 'Images')
+    if not os.path.exists(image_folder):
+        os.makedirs(image_folder)
+    
+    image_path = os.path.join(image_folder, 'image.jpg')
+    
+    camera.start_preview()
+    sleep(5)
+    camera.capture(image_path)
+    camera.stop_preview()
+    
+    send_email(image_path)
+    
+    os.remove(image_path)
 
 while True:
-    key = input("Drücke 1, um ein Foto aufzunehmen: ")
-    if key == "1":
-        camera.capture('/home/pi/Desktop/image.jpg')
-        send_mail(email_sender, email_recipient, "Foto von Raspberry Pi",
-                  "Hier ist das Foto von deinem Raspberry Pi!",
-                  ["/home/pi/Desktop/image.jpg"],
-                  username=email_username, password=email_password)
+    key = input('Press 1 to capture an image: ')
+    
+    if key == '1':
+        capture_image()
